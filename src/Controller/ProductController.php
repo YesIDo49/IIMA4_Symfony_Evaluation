81d706ef496cefbda321c7413d60bfe2674f2565
Joanne <90553540+joannemas@users.noreply.github.com>
2024-03-27 14:39:55 +0100

    :sparkles: add product crud

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class ProductController extends AbstractController
+{
+    #[Route('/', name: 'app_product_index')]
+    public function index(ProductRepository $productRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $productRepository->findAll(),
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_product_show')]
+    public function show(Product $product, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_product_show', [
+                'id' => $product->getId()
+            ]);
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/product/delete/{id}', name: 'app_product_delete')]
+    public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($product);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
