49f1ae1a5e1fa513801457f81ba63576e0d91a65
Nyssa0 <hassinaayachi@hotmail.com>
2024-03-27 16:31:13 +0100

    :sparkles: update controller

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -10,36 +10,55 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 #[Route('/cart')]
 class CartController extends AbstractController
 {
+
     #[Route('/', name: 'app_cart_index', methods: ['GET'])]
     public function index(CartRepository $cartRepository): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'carts' => $cartRepository->findAll(),
-        ]);
+        $cart = $cartRepository->findOneBy(
+          ['user' => $this->getUser(), 'state' => false],
+          ['id' => 'DESC']
+        );
+
+        if (!$cart) {
+          return $this->redirectToRoute('app_cart_new', [], Response::HTTP_SEE_OTHER);
+        } else {
+          return $this->render('cart/index.html.twig', [
+            'cart' => $cart
+          ]);
+        }
+    }
+
+    #[IsGranted('ROLE_SUPER_ADMIN')]
+    #[Route('/all', name: 'app_cart_show_all', methods: ['GET'])]
+    public function showAll(CartRepository $cartRepository): Response
+    {
+      return $this->render('cart/index.html.twig', [
+        'carts' => $cartRepository->createQueryBuilder('c')
+          ->where('c.state = 0')
+          ->orderBy('c.purchase_date', 'DESC')
+          ->getQuery()
+          ->getResult()
+      ]);
     }
 
     #[Route('/new', name: 'app_cart_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $cart = new Cart();
-        $form = $this->createForm(CartType::class, $cart);
-        $form->handleRequest($request);
+      $cart = new Cart();
+      $cart->setState(false);
+      $cart->setUser($this->getUser());
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($cart);
-            $entityManager->flush();
+      $entityManager->persist($cart);
+      $entityManager->flush();
 
-            return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('cart/new.html.twig', [
-            'cart' => $cart,
-            'form' => $form,
-        ]);
+//      return $this->redirectToRoute('app_cart_show', ["id" => $cart->getId()], Response::HTTP_SEE_OTHER);
+      return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
     }
 
     #[Route('/{id}', name: 'app_cart_show', methods: ['GET'])]
@@ -53,29 +72,41 @@ class CartController extends AbstractController
     #[Route('/{id}/edit', name: 'app_cart_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Cart $cart, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(CartType::class, $cart);
-        $form->handleRequest($request);
+      $form = $this->createForm(CartType::class, $cart);
+      $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+      if ($form->isSubmitted() && $form->isValid() && !$cart->isState()) {
+          $entityManager->flush();
 
-            return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
-        }
+          return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
+      }
 
-        return $this->render('cart/edit.html.twig', [
-            'cart' => $cart,
-            'form' => $form,
-        ]);
+      return $this->render('cart/edit.html.twig', [
+          'cart' => $cart,
+          'form' => $form,
+      ]);
     }
 
+    #[Route('/{id}/pay', name: 'app_cart_pay', methods: ['GET', 'POST'])]
+    public function pay(Cart $cart, EntityManagerInterface $entityManager): Response
+    {
+      $cart->setState(true);
+      $cart->setPurchaseDate(new \DateTime("now", new \DateTimeZone("Europe/Paris")));
+
+      $entityManager->flush();
+
+      return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}', name: 'app_cart_delete', methods: ['POST'])]
     public function delete(Request $request, Cart $cart, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$cart->getId(), $request->getPayload()->get('_token'))) {
-            $entityManager->remove($cart);
-            $entityManager->flush();
-        }
+      if ($this->isCsrfTokenValid('delete'.$cart->getId(), $request->getPayload()->get('_token'))) {
+          $entityManager->remove($cart);
+          $entityManager->flush();
+      }
 
-        return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
+      return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
     }
 }
