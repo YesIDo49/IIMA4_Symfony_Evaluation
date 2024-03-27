20b3b24a637f0c8027a9b563af7a19490f9065ce
YesIDo49 <mehdialsidcheikh@gmail.com>
2024-03-27 12:02:27 +0100

    :sparkles: add registration

--- src/Security/AuthAuthenticator.php
+++ src/Security/AuthAuthenticator.php
@@ -48,9 +48,8 @@ class AuthAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+         return new RedirectResponse($this->urlGenerator->generate('app_cart_index'));
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
